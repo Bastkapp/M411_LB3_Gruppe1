1453a9802748fef07239afecc130815a916327d9
Bastian Kappeler <60946803+BeeTheKay@users.noreply.github.com>
2021-01-21 22:31:55 +0100

    Turned BubbleSort

--- src/sorting/algorithms/bubblesort/BubbleSort.java
+++ src/sorting/algorithms/bubblesort/BubbleSort.java
@@ -30,7 +30,7 @@ public class BubbleSort implements ISortAlgorithm {
     int len = array.arraySize();
     for (int i = 0; i < len - 1; i++) {
       for (int j = 0; j < len - i - 1; j++) {
-        if (array.getValue(j) < array.getValue(j + 1)) {
+        if (array.getValue(j) > array.getValue(j + 1)) {
           array.swap(j, j + 1);
           changes++;
         }
