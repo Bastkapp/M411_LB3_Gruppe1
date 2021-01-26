577ce5c5577bc1c61b7de54f64002e434ae89521
Dennis Künzle <kuenzled@bzz.ch>
2021-01-26 12:08:53 +0100

    Gnome Sort Algorithmus

--- src/sorting/algorithms/gnomesort/GnomeSort.java
+++ src/sorting/algorithms/gnomesort/GnomeSort.java
@@ -7,31 +7,69 @@ import sorting.SortArray;
  * Gnome Sort Algorithmus
  *
  * @author Dennis Künzle
+ * @version 1.0
  */
 
-public class GnomeSort {
+public class GnomeSort implements ISortAlgorithm{
 
-    public static int[] gnomeSort(int list[]){
+    private double duration;
+    private int comparison = 0;
+    private int loopRun = 0;
+    private long memory = 0;
+    /**
+     *
+     */
+    @Override
+    public void runSort(SortArray array) {
+        Runtime runtime = Runtime.getRuntime();
+
+        double startTime = System.currentTimeMillis();
+
         int index = 0;
-
-        while (index < list.length) {
-           if (index == 0) {
-               index++;
-           }
-           if (list[index] >= list[index - 1]) {
-               index++;
-           }
-           else {
-               int temp = 0;
-               temp = list[index];
-               list[index] = list[index - 1];
-               list[index - 1] = temp;
-               index --;
-           }
+        while (index < array.arraySize()) {
+            comparison++;
+            if (index == 0) {
+                index++;
+            } else {
+                comparison++;
+                if (array.getValue(index) >= array.getValue(index - 1)) {
+                    index++;
+                } else {
+                    array.swap(index, index - 1);
+                    index--;
+                }
+            }
+            loopRun++;
         }
+        double endTime = System.currentTimeMillis();
 
-        return list;
+        this.duration = endTime - startTime;
+
+        this.memory = runtime.totalMemory() - runtime.freeMemory();
     }
 
+    @Override
+    public String getName() {
+        return "GnomeSort";
+    }
 
+    @Override
+    public double getDuration() {
+        return this.duration;
+    }
+
+    @Override
+    public int getAmountOfComparisons() {
+        return comparison;
+    }
+
+    @Override
+    public long getMemoryUsage() {
+        return memory;
+    }
+
+    @Override
+    public long getLoopRuns() {
+        return loopRun;
+    }
 }
