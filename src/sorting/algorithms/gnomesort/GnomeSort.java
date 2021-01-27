d7c6ab735d3ed3ed9492bdf2b06517e6575fa82e
Dennis Künzle <kuenzled@bzz.ch>
2021-01-27 10:37:46 +0100

    Komentare fertig

--- src/sorting/algorithms/gnomesort/GnomeSort.java
+++ src/sorting/algorithms/gnomesort/GnomeSort.java
@@ -7,7 +7,24 @@ import sorting.SortArray;
  * Gnome Sort Algorithmus
  *
  * @author Dennis Künzle
+ * @date 21.01.2021
  * @version 1.0
+ *
+ * Der Gnome Sort Algorithmus basiert auf dem Prinzip, dass ein Gartenzwerg(Gnome) Blumentöpfe der Grösse nach sortiert.
+ * Der "Zwerg" beginnt ganz links und arbeitet sich nach recht vor. Dabei schaut er sich immer zwei Töpfe an.
+ * Stimmt die Reihenfolge, geht er einen Topf weiter und vergleicht die wieder miteinander.
+ * Wenn die Reihenfolge nicht stimmt, also der linke Topf ist grösser als der rechte Topf so werden diese ausgetauscht.
+ *
+ * Die Struktur ist sehr simpel aufgebaut. Es gibt eine while Schleife die von der ersten Zahl bis zur letzten Zahl durcharbeitet.
+ * in der while schlaufe befinden sich zwei if und eine else Schlaufe. Die sind zum Überprüfen ob die linke oder rechte Zahl grösser ist.
+ * Die Schleifen haben alle drei Zenarien umgesetzt: was wenn die Zahlen gleich gross sind, was wenn die linke Zahl kleiner ist als die rechte und was wenn die linke Zahl grösser ist als die rechte.
+ * Wenn die Reihenfolge falsch ist geht der "Gnome" nach links bis der Topf an der richtigen Stelle ist.
+ *
+ * Der Gnome Sort gehört zu der Kathegorie der stabilen Algorithmen.
+ *
+ * Best-case: O(n)
+ * Worst-case: O(n^2)
+ * Average-case: O(n^2)
  */
 
 public class GnomeSort implements ISortAlgorithm{
@@ -16,26 +33,7 @@ public class GnomeSort implements ISortAlgorithm{
     private int comparison = 0;
     private int loopRun = 0;
     private long memory = 0;
-    /**
-     * Der Gnome Sort Algorithmus basiert auf dem Prinzip, dass ein Gartenzwerg(Gnome) Blumentöpfe der Grösse nach sortiert.
-     * Der "Zwerg" beginnt ganz links und arbeitet sich nach recht vor. Dabei schaut er sich immer zwei Töpfe an.
-     * Stimmt die Reihenfolge, geht er einen Topf weiter und vergleicht die wieder miteinander.
-     * Wenn die Reihenfolge nicht stimmt, also der linke Topf ist grösser als der rechte Topf so werden diese ausgetauscht.
-     *
-     * Die Struktur ist sehr simpel aufgebaut. Es gibt eine while Schleife die von der ersten Zahl bis zur letzten Zahl durcharbeitet.
-     * in der while schlaufe befinden sich zwei if und eine else Schlaufe. Die sind zum Überprüfen ob die linke oder rechte Zahl grösser ist.
-     * Die Schleifen haben alle drei Zenarien umgesetzt: was wenn die Zahlen gleich gross sind, was wenn die linke Zahl kleiner ist als die rechte und was wenn die linke Zahl grösser ist als die rechte.
-     * Wenn die Reihenfolge falsch ist geht der "Gnome" nach links bis der Topf an der richtigen Stelle ist.
-     *
-     * Der Gnome Sort gehört zu der Kathegorie der stabilen Algorithmen.
-     *
-     * Best-case: O(n)
-     * Worst-case: O(n^2)
-     * Average-case: O(n^2)
-     *
-     * @param array Array, das sortiert werden muss.
-     * @see SortArray
-     */
+
     @Override
     public void runSort(SortArray array) {
         Runtime runtime = Runtime.getRuntime();
