ab8174fda5688d4d5dae46c18d93f4c4fe2d2a5d
Marko Micanovic <micanovicm@bzz.ch>
2021-01-28 15:34:55 +0100

    Code fertig

--- src/sorting/algorithms/selectionsort/Selectionsort.java
+++ src/sorting/algorithms/selectionsort/Selectionsort.java
@@ -7,10 +7,10 @@ import sorting.algorithms.ISortAlgorithm;
  * @Author: Marko Micanovic
  * @Date: 21.01.2021
  * @Version: 1.0
- *
+ * <p>
  * Hier habe ich den SelectionSort übernommen, welchen wir im Moodle am 14.01 behandelt haben.
  * Der Code wurde einfach so angepasst, dass es noch die Variablen für Zeit, Speicherverbrauch und Anzahl Durchläufe hat.
- *
+ * <p>
  * Ablauf:
  * Der Algorithmus funktioniert so, dass er die erste Zahl als lowestNumber festlegt. Die Variable currentNumber geht jede
  * Position im array durch und prüft, ob lowestNumber < currentNumber übereinstimmt. Falls das aber nicht übereinstimmt, wird
@@ -18,19 +18,18 @@ import sorting.algorithms.ISortAlgorithm;
  * durchläufe wird hier hochgezählt. Wenn sich lowestNumber dann nicht mehr ändert und currentNumber die ganze Liste durch-
  * gegangen ist, dann wird am schluss der Wert von der ersten Position mit dem kleinsten Wert vertauscht und die Länge des
  * arrays verkürzt sich somit um 1 Eintrag, sodass die sortierten Werte nicht mehrmals überprüft werden.
- *
- *
+ * <p>
+ * <p>
  * Best-, Worst- und Average-Case:
  * Der Selectionsort geht immer die ganze Liste durch, weshalb Best-, Worst- und Average-Case auch alle den gleichen Wert
  * haben. Der Sort geht immer eine fixe Anzahl vergleiche durch, weshalb sich die Fälle von der Zeit her nicht unterscheiden.
  * Auch wenn die Liste schon ganz sortiert wäre, würde der Sort durch das ganze Array gehen. Also ist die Zeitkomplexität für
  * alle 3 Fälle O(n^2).
- *
- *
+ * <p>
+ * <p>
  * Stabilität:
  * Der Algorithmus ist nicht stabil, weil die Werte während dem durchlaufen immer wieder vertauscht werden und sich deshalb
  * die Reihenfolge von gleichen Werten ändern kann.
- *
  */
 
 public class Selectionsort implements ISortAlgorithm {
@@ -44,7 +43,6 @@ public class Selectionsort implements ISortAlgorithm {
     public String getName() {
         return "SelectionSort";
     }
-
 
 
     @Override
